3715ef226a6272c8bdb15bb6dcfdd01c87b23e53
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 22:58:10 +0530

    add : Extend maximum method take more than three parameter

--- src/com/blz/day13/FindMaximum.java
+++ src/com/blz/day13/FindMaximum.java
@@ -1,22 +1,29 @@
 package com.blz.day13;
 
-public class FindMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Problem using Generics");
-		String e = "Ahmednagar", f = "Pune" , g = "Mumbai";
-        testMaximum (e,f,g);
+public class FindMaximum <T extends Comparable<T>> {
+
+    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+        T max = val1;
+        if (val2.compareTo(max) > 0 ) {
+            max = val2;
+        } if (val3.compareTo(max) > 0){
+            max = val3;
+        }if (val4.compareTo(max) > 0 ){
+            max = val4;
+        }if (val5.compareTo(max) > 0){
+            max = val5;
+
+        }
+        return max;
     }
 
-    private static void testMaximum(String e, String f, String g) {
-    	 String max = e;
-	        if(f.compareTo(e)>0){
-	            max = f;
-	        }
-	        if(g.compareTo(max)>0){
-	            max = g;
-	        }
-	        System.out.println("Maximum Number is: "+max);
-    	
-	}
+    public static void main(String[] args) {
+        
 
+        FindMaximum max = new FindMaximum();
+        System.out.println( "The maximum value is :: "+maximumValue(2.5f,3.5f,4.5f,5.5f,6.5f));
+
+
+
+    }
 }

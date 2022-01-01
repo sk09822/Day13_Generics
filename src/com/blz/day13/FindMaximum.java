d84c2999cff54e5a4436032816c6056a2c520f20
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 22:18:40 +0530

    add : find maximum String

--- src/com/blz/day13/FindMaximum.java
+++ src/com/blz/day13/FindMaximum.java
@@ -1,22 +1,22 @@
 package com.blz.day13;
 
 public class FindMaximum {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Find Maximum Problem using Generics");
-		Float p = 20.85f, q = 90.55f, r = 80.98f;
-		testMaximum(p, q, r);
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Problem using Generics");
+		String e = "Ahmednagar", f = "Pune" , g = "Mumbai";
+        testMaximum (e,f,g);
+    }
 
-	private static void testMaximum(Float p, Float q, Float r) {
-		Float max = p;
-		if (q.compareTo(p) > 0) {
-			max = q;
-		}
-		if (r.compareTo(max) > 0) {
-			max = r;
-		}
-		System.out.println("Maximum Number is: " + max);
-
+    private static void testMaximum(String e, String f, String g) {
+    	 String max = e;
+	        if(f.compareTo(e)>0){
+	            max = f;
+	        }
+	        if(g.compareTo(max)>0){
+	            max = g;
+	        }
+	        System.out.println("Maximum Number is: "+max);
+    	
 	}
 
 }

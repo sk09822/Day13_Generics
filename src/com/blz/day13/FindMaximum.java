ab109456397ae7d8ec49e56b763556cf4b7c8696
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 22:00:02 +0530

    Three to One Generic Method and Find Max

--- src/com/blz/day13/FindMaximum.java
+++ src/com/blz/day13/FindMaximum.java
@@ -1,8 +1,32 @@
 package com.blz.day13;
 
 public class FindMaximum {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics Program");
-}
+
+		System.out.println("Welcome to Generic Program.");
+		Integer a = 20, b = 42, c = 65;
+		Float x = 2.3f, y = 7.8f, z = 6.7f;
+		String l = "Apple", m = "Banana", n = "Orange";
+
+		FindMaximum.getMaximum(a, b, c);
+		FindMaximum.getMaximum(x, y, z);
+		FindMaximum.getMaximum(l, m, n);
+	}
+
+	private static <T extends Comparable> void getMaximum(T a, T b, T c) {
+
+		T max = a;
+		if (b.compareTo(a) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
+		}
+
+		System.out.println("Maximum ::" + max);
+
+	}
+
 }

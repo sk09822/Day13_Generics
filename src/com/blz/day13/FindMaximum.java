3e9357121eea5f63d84d9ebca31425ac0f068503
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-01 22:09:43 +0530

    add : find maximum integer

--- src/com/blz/day13/FindMaximum.java
+++ src/com/blz/day13/FindMaximum.java
@@ -1,42 +1,22 @@
 package com.blz.day13;
 
-public class FindMaximum<T extends Comparable> {
-	T var1, var2, var3;
+public class FindMaximum {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Find Maximum Problem using Generics");
+	        Integer a = 20, b = 50, c =80;
+	        testMaximum (a,b,c);
+	    }
 
-	public FindMaximum(T var1, T var2, T var3) {
-		this.var1 = var1;
-		this.var2 = var2;
-		this.var3 = var3;
-	}
+	    private static void testMaximum(Integer a, Integer b, Integer c) {
+	        Integer max = a;
+	        if(b.compareTo(a)>0){
+	            max = b;
+	        }
+	        if(c.compareTo(max)>0){
+	            max = c;
+	        }
+	        System.out.println("Maximum Number is::"+max);
+	    }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Generic Program.");
-
-		Integer a = 21, b = 42, c = 67;
-		Float x = 3.5f, y = 9.8f, z = 5.8f;
-		String l = "Apple", m = "Banana", n = "Orange";
-
-		new FindMaximum<>(a, b, c).max();
-		new FindMaximum<>(x, y, z).max();
-		new FindMaximum<>(l, m, n).max();
-	}
-
-	private void max() {
-		FindMaximum.getMaximum(var1, var2, var3);
 
 	}
-
-	private static <T extends Comparable> void getMaximum(T a, T b, T c) {
-
-		T max = a;
-		if (b.compareTo(a) > 0) {
-			max = b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = c;
-		}
-
-		System.out.println("Maximum ::" + max);
-
-	}
-}
